26f6675fda9678629db1a81e61986bdf0099f719
Rodrigo Martín <frontera121@gmail.com>
2016-11-18 12:50:05 +0100

    Prueba algoritmo con peones
    Se ha importado el debugger. Error con funcion score en piezas.py/peon

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import pdb
 
 from tkinter import *
 import table as tab
-import piezas as p
+import piezas as peon
 
 #master = Tk()
 
@@ -14,21 +14,40 @@ import piezas as p
 
 #Crea el tablero
 T = tab.table()
-p1 = p.peon()
-p1.pos=[1,0]
 
+
+#
 #for i in range (8):             #
 #    for j in range(8):          #cf
 #        w.create_rectangle(50+T[i,j].pos_i*75, 650-T[i,j].pos_j*75,
 #                           50+(T[i,j].pos_i + 1)*75, 650-(T[i,j].pos_j+1)*75,
 #                           fill=T[i,j].color) # Crea la cuadricula, el signo - despues de 650 es para que vayan de abajo a arriba
-print (p1.pos)
-p1.influence()
-print (p1.influence_pos)
-p1.posible_move()
 
-print (p1.pos)
-p1.influence()
-print (p1.influence_pos)
+
+#Colocacion de todos lpos peones en la fila 2
+p = []
+for i in range(8):
+	p.append(peon.peon())
+	p[i].name = 'p' + str(i)
+	p[i].pos  = [1,i]
+
+	T[p[i].pos[0],p[i].pos[1]].pieza = p[i].name 	
+#
+#
+#Prueba de algoritmo con peones
+score = []
+temp_score = []
+max_score = [0,0]
+
+pdb.set_trace()
+
+for a in range(8):
+	p[a].posible_move()
+	p[a].temp_influence()
+	p[a].score()
+	if p[a].score > max_score:
+		max_score = p[a].temp_pos
+
+print (max_score)
 
 #Fin de creacion del tablero
--- piezas.py
+++ piezas.py
@@ -8,36 +8,40 @@ class peon(object):
 	influence_pos = []
 	name = 'peon'
 	score = 0
+	temp_pos = []
 
 	def posible_move(self):
 		temp = []
 		temp2 = []
 		temp = [self.pos[0] + 1 , self.pos[1]] #El valor que debe aumentar en el movimiento del peon debe ser el valor i (eje x) para que cambie de fila a una superior
-		self.pos=[]
+		self.temp_pos=[]
 		for i in range(len(temp)):
 			if (temp[i]>=0 and temp[i]<=7):
 				temp2.append(temp[i])
 
-		self.pos = temp2
-		T[self.pos[0], self.pos[1]].pieza = self.name
-
-	def influence (self):
-		temp = []
-		temp = [[self.pos[0]+1,self.pos[1]+1],[self.pos[0]-1,self.pos[1]-1]]
+		self.temp_pos = temp2
 		
+
+	def temp_influence (self):
+		temp = []
+		temp = [[self.temp_pos[0]+1,self.temp_pos[1]+1],[self.temp_pos[0]+1,self.temp_pos[1]-1]]
+		self.influence_pos = []
 		for i in range(len(temp)):
 			j=0
 			if (temp[i][j]>=0 and temp[i][j]<=7 and temp[i][j+1]>=0 and temp[i][j]<=7):
-				self.influence_pos.append(temp[i][j])
+				self.influence_pos.append(temp[i])
 
 
 	def score (self):
-		score = 0
+		self.score = 0
 		for i in range((len(self.influence_pos))):
-			score = self.valor * T[self.influence_pos[i]].value + score
-			score = score + self.valor*T[self.pos].value
-			T[self.influence_pos[i][0],self.influence_pos[i][1]].influencia = score
+			j = 0
+			self.score = self.valor * T[self.influence_pos][i].value + self.score
+			self.score = self.score + self.valor*T[self.pos].value
+		#	T[self.influence_pos[i][0],self.influence_pos[i][1]].influencia = score
 					
+	def move(slef):
+		self.pos = self.temp_pos
 
 
 
